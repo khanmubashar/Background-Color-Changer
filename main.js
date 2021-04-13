a5a1bb76fa55817416f653bb6b1753360a51520c
Mubashar Khan <khanmubashar678@gmail.com>
2021-04-14 02:11:58 +0500

    Latest Files 	new file:   index.html 	new file:   main.css 	new file:   main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+const btn = document.getElementById('changecolorbtn');
+
+btn.addEventListener('click', changeColor);
+
+function changeColor() {
+  debugger;
+  const colorText = document.querySelector('.colorcontainer_colortitle');
+  const backgroundColor = document.querySelector('.colorcontainer');
+  backgroundColor.style.background = getRandomColor();
+  colorText.innerText = getRandomColor();
+
+}
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
