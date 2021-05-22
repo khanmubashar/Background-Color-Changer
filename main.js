1c8580c0cddd6a0eabfa1454c69eeca78b8614e4
Mubashar Khan <mubasharkhan@jinnbyte.com>
2021-05-22 15:27:22 +0500

    My new Changes

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ function changeColor() {
 }
 
 function getRandomColor() {
-  const letters = '0123456789ABCDEF';
+
+  const letters33 = '0123456789ABCDEF';
   let color = '#';
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
